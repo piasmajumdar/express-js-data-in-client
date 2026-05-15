fa9402b69b44f938f78d9c715d07ce6b5cd168b1
piasmajumdar <piasmajumdar@gmail.com>
2026-05-16 01:11:01 +0530

    first commit

---
+++ src/app/users/page.jsx
@@ -0,0 +1,22 @@
+const UsersPage = async () => {
+    const res = await fetch('http://localhost:8000/users');
+    const users = await res.json();
+    console.log(users);
+    return (
+        <div>
+            <h2>Users: {users.length}</h2>
+
+            <div className="grid grid-cols-3 gap-4">
+                {
+                    users.map(user => <div key={user.id} className="border-2 p-3 rounded-md">
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                    </div>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default UsersPage;
